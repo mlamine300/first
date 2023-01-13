23eb77eaf10490f9bccb164c2dae37b1ed74c0e5
laouf <laouf@lamine-hp>
2023-01-13 23:57:03 +0100

    x

--- git/src/git/anything.java
+++ git/src/git/anything.java
@@ -6,7 +6,8 @@ public class anything {
 		// TODO Auto-generated method stub
 
 		System.out.println("from eclipse");
-		System.out.println("from eclipse 2");
+		System.out.println("today is new day");
+	
 	}
 
 }
