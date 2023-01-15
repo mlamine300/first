f54d30980430c646be9bd174c5fb2e3569e34cd5
WBD1DDG <WBD1DDG@WKSP00103F43.europe.ups.com>
2023-01-15 10:33:37 +0100

    asalem 3likom

--- git/src/git/anything.java
+++ git/src/git/anything.java
@@ -6,6 +6,12 @@ public class anything {
 		// TODO Auto-generated method stub
 
 		System.out.println("from eclipse");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("today is new day");
 	
 	}
